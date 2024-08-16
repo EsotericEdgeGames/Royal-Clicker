7e1dd11954b24f14f2358782defb203850dac8bb
JuanSebas572 <juanseba270303@gmail.com>
2024-08-16 19:01:49 -0300

    escribi el codigo en español y añadi la variable cpc para modificar a futuro

--- Control.gd
+++ Control.gd
@@ -2,18 +2,21 @@ extends Control
 
 # Variable para contar los clicks.
 var puntos: int = 0
+var cpc: int = 1
 
 # Función que se ejecuta cuando se hace click en el botón.
-func _on_button_pressed():
-	puntos += 1
+func boton_presionado():
+	puntos += cpc
 	actualizar_puntos()
 
 
 # Función para actualizar el texto del Label.
 func actualizar_puntos():
 	print(puntos)
-
 # Conectar la señal del botón correctamente.
+
+
+
 
 
 
