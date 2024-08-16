dcbae9349f9563bb2e43d8f59b5494a4e68eca61
GSAniki <alexdanielmoran833@gmail.com>
2024-08-16 17:04:20 -0300

    Un par de correcciones

--- node_3d.gd
+++ node_3d.gd
@@ -1,18 +1,13 @@
 extends Node3D
 
-var clicks = 0 #Varible Clicks que se va a usar en gran parte de los Nodos
-var segs = 0 #Varible Segundos para usar con AutoClicker
-var x = 0.25 #Variable de Clicks Extras (por el autoclicker) WIP
-var segs2 = 0 #Variable que se va a ir comparando con los segundos
-
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	#$Label.text = str(clicks) #Click esta en int y se escribe a str par que se vea como texo
+var clicks = 0 
+var cps = 0.25 
+var segs2 = 0 
 
 
-func _process(delta):
+func _process():
 	var segs = Time.get_ticks_msec() / 1000
 	if segs > segs2:
-		clicks += x 
+		clicks += cps 
 		segs += 0.25 
 		print(clicks)
