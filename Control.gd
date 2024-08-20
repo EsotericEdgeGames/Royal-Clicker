bef375293d01efcc3e0f2b4205f8734740c72af4
JuanSebas572 <juanseba270303@gmail.com>
2024-08-20 18:51:19 -0300

    fixie mi codigo otra vez

--- Control.gd
+++ Control.gd
@@ -1,18 +1,2 @@
-extends Control
 
-
-# Variable para contar los clicks.
-var puntos: int = 0
-var cpc: int = 1
-
-
-# Función que se ejecuta cuando se hace click en el botón.
-func boton_presionado():
-	puntos += cpc
-	actualizar_puntos()
-
-
-func actualizar_puntos():
-	print(puntos)
-# Conectar la señal del botón correctamente.
 
