0e13e8bffab05aaaa7959479a557fdae8174bba2
JuanSebas572 <juanseba270303@gmail.com>
2024-08-20 18:15:14 -0300

    fixie mi codigo

--- Control.gd
+++ Control.gd
@@ -1,8 +1,10 @@
 extends Control
 
+
 # Variable para contar los clicks.
 var puntos: int = 0
 var cpc: int = 1
+
 
 # Función que se ejecuta cuando se hace click en el botón.
 func boton_presionado():
@@ -10,17 +12,7 @@ func boton_presionado():
 	actualizar_puntos()
 
 
-# Función para actualizar el texto del Label.
 func actualizar_puntos():
 	print(puntos)
 # Conectar la señal del botón correctamente.
-
-
-
-
-
-
-
-
-
 
