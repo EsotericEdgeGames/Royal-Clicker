89302fad3db9c4c57e361b8ea9e995b2b4e4ffbf
GSAniki <alexdanielmoran833@gmail.com>
2024-08-26 12:12:14 -0300

    Ahora si lo mando desde mi rama,conchadesumadre

--- node_3d.gd
+++ node_3d.gd
@@ -1,13 +1,29 @@
 extends Node3D
 
+
 var clicks = 0 
 var cps = 0.25 
 var segs2 = 0 
+var cpc: int = 1
+var messi = 3
 
 
 func _process(delta):
-	var segs = Time.get_ticks_msec() / 1000
-	if segs > segs2:
-		clicks += cps 
-		segs += 0.25 
+#	var segs = Time.get_ticks_msec() / 1000
+#	if segs>segs2:
+#		clicks += cps 
+#		segs += 0.25 
+#		print(clicks)
+	pass
+
+
+# Función que se ejecuta cuando se hace click en el botón.
+func boton_presionado():
+	clicks += cpc
+	print(clicks)
+	
+
+func _on_button_button_down():
+		clicks += cpc
 		print(clicks)
+	
