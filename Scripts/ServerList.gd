f26ab60bc90484b173a52f684234121ac13cbcd5
GSAniki <alexdanielmoran833@gmail.com>
2024-09-03 13:48:09 -0300

    Autoclick Fixeado.Agrego data en el comentario

---
+++ Scripts/ServerList.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func menu_presionado():
+	get_tree(). change_scene_to_file("res://Escenas/menù.tscn")
---
+++ Scripts/node_3d.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+
+
+var clicks = 0 
+var cps: int = 1 
+var segs2 = 0 
+var cpc: int = 1
+var falso_delta = 0
+
+
+func _process(delta):
+	falso_delta += delta
+	if falso_delta >= 1:
+		clicks+=cps
+		falso_delta = 0
+		print(clicks)
+
+
+func menu_presionado():
+	get_tree(). change_scene_to_file("res://Escenas/menù.tscn")
+
+func _boton_presionado():
+		clicks += cpc
+		print(clicks)
+
