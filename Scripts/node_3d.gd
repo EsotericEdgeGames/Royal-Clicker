007d7e3857051d929285d65240cb5db9c7abc39c
GSAniki2 <moranalex446@gmail.com>
2024-09-05 11:31:42 -0300

    Autoclick fixeado.Manualclick dejo de funcionar por defecto

--- Scripts/node_3d.gd
+++ Scripts/node_3d.gd
@@ -1,24 +1,19 @@
 extends Node3D
 
 
-
 var clicks = 0 
-var cps = 0.25 
+var cps = 1
 var segs2 = 0 
 var cpc: int = 1
+var falso_delta= 0
 
 
 func _process(delta):
-	var segs = Time.get_ticks_msec() / 1000
-	if segs>segs2:
-		clicks += cps 
-		segs += 0.25 
-
-
-
-# Función que se ejecuta cuando se hace click en el botón.
-func boton_presionado():
-	clicks += cpc
+	falso_delta += delta
+	if falso_delta >= 1:
+		clicks+=cps
+		falso_delta = 0
+		print(clicks)
 
 
 func menu_presionado():
