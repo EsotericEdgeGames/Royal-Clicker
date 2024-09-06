619521bdbfa6c98e21387ba203520a4cbcdcd4ff
GSAniki2 <moranalex446@gmail.com>
2024-09-06 18:57:15 -0300

    Agregue la funcion pedida ( _physics_process()). falso_delta --> Frame. Y borre la variable segs2 ya que no se usaba

--- Scripts/node_3d.gd
+++ Scripts/node_3d.gd
@@ -3,19 +3,17 @@ extends Node3D
 
 
 var clicks = 0 
-var cps = 0.25 
-var segs2 = 0 
+var cps = 1
 var cpc: int = 1
-var falso_delta = 1
+var frame = 1
 
 
-func _process(delta):
-	falso_delta += delta
-	if falso_delta >= 1:
+func _physics_process(delta):
+	frame += delta
+	if frame >= 1:
 		clicks+=cps
-		falso_delta = 0
+		frame = 0
 		print(clicks) 
-
 
 
 # Función que se ejecuta cuando se hace click en el botón.
@@ -25,6 +23,7 @@ func boton_presionado():
 
 func menu_presionado():
 	get_tree(). change_scene_to_file("res://Escenas/menù.tscn")
+
 
 func _boton_presionado():
 		clicks += cpc
