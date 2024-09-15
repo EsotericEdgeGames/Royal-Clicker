369afca8c5a7d938b836d83066732fe0a86f09e3
JuanSebas572 <juanseba270303@gmail.com>
2024-09-15 18:14:37 -0300

    fix de la escena con cd

--- Scripts/node_3d.gd
+++ Scripts/node_3d.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
 
-
 var clicks = 0 
 var cps = 1
 var cpc: int = 1
 var frame = 1
+var cooldown: float = 0.15  # Duración del cooldown en segundos
+var tiempo_actual: float = 0.0  # Tiempo acumulado para controlar el cooldown
+
+@onready var boton = $Control/Clicker/Boton  # Refiere al nodo del botón
 
 
 func _physics_process(delta):
@@ -14,11 +17,21 @@ func _physics_process(delta):
 		clicks+=cps
 		frame = 0
 		print(clicks) 
+	if boton.disabled:
+		tiempo_actual += delta  # Acumular el tiempo transcurrido
+		if tiempo_actual >= cooldown:  # Si pasa el tiempo del cooldown
+			boton.disabled = false  # Activar el botón visualmente
+			tiempo_actual = 0.0  # Reiniciar el tiempo acumulado
 
+func _ready():
+	boton.pressed.connect(boton_presionado)  # Conectar el botón al evento de presionado
 
-# Función que se ejecuta cuando se hace click en el botón.
+# Función que se ejecuta cada vez que se hace click en el botón
 func boton_presionado():
-	clicks += cpc
+	if not boton.disabled:  # Solo se puede presionar si el botón está habilitado
+		clicks += cpc
+		print(clicks)
+		boton.disabled = true  # Desactivar botón visualmente
 
 
 func menu_presionado():
