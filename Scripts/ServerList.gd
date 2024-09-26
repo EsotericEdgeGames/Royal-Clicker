ec72c1f9a14b87bfcd69f6441c35bff600cb22fa
Elizabeth178 <tania.alarcon578@gmail.com>
2024-09-26 15:44:28 -0300

    EliminarEdicionDeTextos

--- Scripts/ServerList.gd
+++ Scripts/ServerList.gd
@@ -1,9 +1,10 @@
 extends Control
 
-
 func menu_presionado():
-	get_tree(). change_scene_to_file("res://Escenas/menù.tscn")
+	get_tree().change_scene_to_file("res://Escenas/menù.tscn")
 
 func _ready():
-	$TextEdit.editable = false
-	
+	$TextEdit.editable = false 
+
+func _on_menu_pressed():
+	menu_presionado()
